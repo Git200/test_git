286eef0743dc3c3ec106bc9666fb8276169fec0a
tatpetrova <vipstud1@yandex.ru>
2016-11-14 23:49:38 +0300

    Четвертый комментарий

--- src/main/java/com/example/GitTestApplication.java
+++ src/main/java/com/example/GitTestApplication.java
@@ -4,6 +4,7 @@
 
 // Третий комментарий
 // третий комментарий - здесь я исправила
+// Четвертый комментарий
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
