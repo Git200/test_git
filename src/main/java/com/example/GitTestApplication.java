4fe0858cf010bb753517ff3f068d96b75c0a30a3
Sergey <apay1234@mail.ru>
2016-11-15 00:01:15 +0300

    добавлен 8 ком

--- src/main/java/com/example/GitTestApplication.java
+++ src/main/java/com/example/GitTestApplication.java
@@ -8,6 +8,7 @@
 // Пятый комментарий
 // Шестой ком
 // Седьмой коммент
+// восьмой коммент
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
