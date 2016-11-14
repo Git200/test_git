02e8e3247a88e8f3f8d8a2cadbd96696390456af
Sergey <apay1234@mail.ru>
2016-11-14 23:48:52 +0300

    еще коммит

--- src/main/java/com/example/GitTestApplication.java
+++ src/main/java/com/example/GitTestApplication.java
@@ -6,6 +6,7 @@
 // третий комментарий - здесь я исправила
 // Четвертый комментарий
 // Пятый комментарий
+// Шестой ком
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
